3dbc793921679017e5afcc40389f860c27e8958a
Leticia-SS <leticia.saraiva013@gmail.com>
2025-02-10 09:17:07 -0300

    if exercises

--- src/Exercicio11.java
+++ src/Exercicio11.java
@@ -1,7 +1,28 @@
 import java.util.Scanner;
-public class Exercicio4 {
+public class Exercicio11 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        int x1, x2;
+
+        System.out.println("Equação do segundo grau ax^2 + bx + c");
+        System.out.println("Digite o valor de a");
+        int a = scanner.nextInt();
+        System.out.println("Digite o valor de b");
+        int b = scanner.nextInt();
+        System.out.println("Digite o valor de c");
+        int c = scanner.nextInt();
+
+        double delta = Math.pow(b, 2) - (4 * a * c);
+
+        if(delta<0) System.out.println("Equação não possui raizes reais");
+        else if (delta == 0){
+            x1 = -b / (2 * a);
+            System.out.println("Raíz X: "+ x1);
+        } else{
+            x1 = (-b + (int) + Math.sqrt(delta)) / (2 * a);
+            x2 = (-b + (int) - Math.sqrt(delta)) / (2 * a);
+            System.out.println("Raizes: \nX1: " + x1 + "\nX2:" + x2);
+        }
 
     }
 }
