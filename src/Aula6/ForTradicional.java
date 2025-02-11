aaf5e35625c466983dfa44be447a8923dc88ed5e
Leticia-SS <leticia.saraiva013@gmail.com>
2025-02-11 09:02:45 -0300

    aula 6

---
+++ src/Aula6/ForTradicional.java
@@ -0,0 +1,11 @@
+package Aula6;
+
+public class ForTradicional {
+    public static void main(String[] args){
+        for(int k=10; k>0; k--){
+            System.out.println(k);
+
+            System.out.println("escreve o texto + .sout");
+        }
+    }
+}
