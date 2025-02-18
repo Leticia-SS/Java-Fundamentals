7306a5f9524e9bae49181e02d94d52d1e43d0135
leticia.ssilva <leticia.saraiva013@gmail.com>
2025-02-18 09:01:51 -0300

    aula 7

--- src/Aula8/CalculadoraFibonacci.java
+++ src/Aula8/CalculadoraFibonacci.java
@@ -2,9 +2,15 @@ package Aula8;
 import java.util.Scanner;
 public class CalculadoraFibonacci {
     public static void main(String[] args){
-//        long calcular(long n){
-//
-//
-//        }
+        long calcular(long n){
+            int a = 0, b = 1;
+            if(n <= 1) return n;
+            for(int i = 2; i <= n; i++){
+                int next = a + b;
+                a = b;
+                b = next;
+            }
+
+        }
     }
 }
