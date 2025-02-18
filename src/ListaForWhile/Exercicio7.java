4e3179d8a4a5794604ef75a242145dea73b09aaa
Leticia-SS <leticia.saraiva013@gmail.com>
2025-02-17 22:03:37 -0300

    exercises for and while

---
+++ src/ListaForWhile/Exercicio7.java
@@ -0,0 +1,12 @@
+package ListaForWhile;
+import java.util.Scanner;
+public class Exercicio7 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Ditige um número");
+        String num = input.nextLine();
+
+
+    }
+}
