c68645d9483c4a514fa2fa2acbf9825038be02fa
leticia.ssilva <leticia.saraiva013@gmail.com>
2025-02-24 09:09:39 -0300

    aula 7

---
+++ src/Aula9/Strings1.java
@@ -0,0 +1,33 @@
+package Aula9;
+
+public class Strings1 {
+    public static void main(String[] args) {
+        String strg1 = "Lara";
+        String strg2 = "Stephanie";
+        String text = "\n" + "Toda pessoa que já leu um número suficiente de livros," +
+                "\n" + "Como bem descreve Michèle Petit em "A arte de Ler ou "";
+
+       // Pattern Flyweight
+       int a = 5;
+       int b = 5;
+       char c1 = 'a';
+       char c2 = 'a';
+
+       if(a == b) System.out.println("a == b");
+       if(c1 == c2) System.out.println("c1 == c2");
+
+//       Aluno a1 = new Aluno();
+//       ai.nome = "Leonardo";
+
+//        Aluno a2 = new Aluno();
+//        a2.nome = "Lara";
+
+//        if(a1 == a2){
+//            System.out.println("Iguais");
+//        } else {
+//            System.out.println("Diferentes");
+//        }
+
+
+    }
+}
