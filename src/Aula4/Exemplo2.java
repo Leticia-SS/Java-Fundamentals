9afc27605a2beb105e3d52fd007ed1e1e6fd1bcf
Leticia-SS <leticia.saraiva013@gmail.com>
2025-03-11 21:35:52 -0300

    aula 4

--- src/Aula4/Exemplo2.java
+++ src/Aula4/Exemplo2.java
@@ -2,7 +2,13 @@ package Aula4;
 
 public class Exemplo2 {
     public static void main(String[] args){
-        short age = 50;
-        boolean valid = true;
+        short idade = 50;
+        boolean conviteValido = true;
+        boolean maiorIdade = (idade >= 18);
+        if(maiorIdade && conviteValido){
+            System.out.println("Voce pode entrar na festa");
+        }else {
+            System.out.println("Voce nao pode entrar na festa");
+        }
     }
 }
