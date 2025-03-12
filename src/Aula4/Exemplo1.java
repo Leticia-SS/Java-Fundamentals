1e740887623d9bcdd73bd5ce2ed0f63493e067f0
Leticia-SS <leticia.saraiva013@gmail.com>
2025-03-11 21:35:13 -0300

    aula 4

---
+++ src/Aula4/Exemplo1.java
@@ -0,0 +1,12 @@
+package Aula4;
+
+public class Exemplo1 {
+    public static void main(String[] args){
+        short age = 12;
+        if(age < 18){
+            System.out.println("Você é menor de idade");
+        } else {
+            System.out.println("Você é maior de idade");
+        }
+    }
+}
---
+++ src/Aula4/Exemplo2.java
@@ -0,0 +1,8 @@
+package Aula4;
+
+public class Exemplo2 {
+    public static void main(String[] args){
+        short age = 50;
+        boolean valid = true;
+    }
+}
